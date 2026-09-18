473c9152ef5a7f4de34a6af2f7978de92af17fc9
F0xer <toespino@42angouleme.fr>
2026-09-18 12:11:07 +0000

    add doccube documentation to data structures

--- inlcudes/type.h
+++ inlcudes/type.h
@@ -6,7 +6,7 @@
 /*   By: toespino <toespino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/17 14:38:21 by toespino          #+#    #+#             */
-/*   Updated: 2026/09/17 16:55:15 by toespino         ###   ########.fr       */
+/*   Updated: 2026/09/18 12:10:18 by toespino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,12 @@
 # include <stdint.h>
 # include <stdbool.h>
 
+// @doc t_coordinate
+// @kind type
+// @desc Representation of coordinates in space.
+// @field x: double, Position on x axis.
+// @field y: double, Position on y axis.
+// @field z: double, Position on z axis.
 typedef struct	s_coodinate
 {
 	double	x;
@@ -23,6 +29,12 @@ typedef struct	s_coodinate
 	double	z;
 }	t_coordinate;
 
+// @doc t_vector
+// @kind type
+// @desc Representation of a vector in space.
+// @field x: double, Vector value on x axis.
+// @field y: double, Vector value on y axis.
+// @field z: double, Vector value on z axis.
 typedef struct	s_vector
 {
 	double	x;
@@ -30,6 +42,12 @@ typedef struct	s_vector
 	double	z;
 }	t_vector;
 
+// @doc t_color
+// @kind type
+// @desc Representation of an RGB color.
+// @field r: int8_t, Red value up to 255.
+// @field g: int8_t, Green value up to 255.
+// @field b: int8_t, Blue value up to 255.
 typedef struct	s_color
 {
 	int8_t	r;
@@ -37,6 +55,13 @@ typedef struct	s_color
 	int8_t	b;
 }	t_color;
 
+// @doc t_plane
+// @kind type
+// @desc Plane representation in an environement.
+// @field type: char *, Object indentification field.
+// @field coordinate: [[t_coordinate]], Coordinates of a point in the plane.
+// @field color: [[t_color]], Plane color.
+// @field normal: [[t_vector]], Plane normal vector.
 typedef struct	s_plane
 {
 	char			*type;
@@ -45,6 +70,13 @@ typedef struct	s_plane
 	t_vector		normal;
 }	t_plane;
 
+// @doc t_sphere
+// @kind type
+// @desc Sphere representation in an environement.
+// @field type: char *, Object indentification field.
+// @field coordinate: [[t_coordinate]], Sphere center coordinates.
+// @field color: [[t_color]], Sphere color.
+// @field radius: double, Sphere radius.
 typedef struct	s_sphere
 {
 	char			*type;
@@ -53,6 +85,15 @@ typedef struct	s_sphere
 	double			radius;
 }	t_sphere;
 
+// @doc t_cylinder
+// @kind type
+// @desc Cylinder representation in an environement.
+// @field type: char *, Object indentification field.
+// @field coordinate: [[t_coordinate]], Cylinder center coordinates.
+// @field color: [[t_color]], Cylinder color.
+// @field diameter: double, Cylinder diameter.
+// @field height: double, Cylinder height.
+// @field normal: [[t_vector]], Cylinder normal vector.
 typedef struct	s_cylinder
 {
 	char			*type;
@@ -63,6 +104,12 @@ typedef struct	s_cylinder
 	t_vector		normal;
 }	t_cylinder;
 
+// @doc t_light
+// @kind type
+// @desc Sketchy light representation.
+// @field coordinate: [[t_coordinate]], Light coordinates.
+// @field color: [[t_color]], Light color.
+// @field brightness: double, Light brightness.
 typedef struct	s_light
 {
 	t_coordinate	coordinate;
@@ -70,12 +117,26 @@ typedef struct	s_light
 	double			brightness;
 }	t_light;
 
+// @doc t_amb
+// @kind type
+// @desc Sketchy ambient light representation.
+// @field color: [[t_color]], Ambient light color.
+// @field brightness: double, Ambient light brightness.
 typedef struct	s_amb
 {
 	t_color	color;
 	double	brightness;
 }	t_amb;
 
+// @doc t_data
+// @kind type
+// @desc Scene representation in a struct, isn't it beatiful ?
+// @field objs: void *, Scene objects array.
+// @field lights: [[t_light]] *, Scene lights array.
+// @field ambient: [[t_amb]], Ambient light specifications.
+// @field fov: int32_t, Fov value for the camera in degres.
+// @field height: int32_t, Window height in pixels.
+// @field width: int32_t, Window width in pixels.
 typedef struct	s_data
 {
 	void	*objs;
